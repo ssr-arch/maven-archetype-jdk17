7623d4c17d09cb645e14e313d25da66b9bbd55ea
ssr-j <hanyubeiwai@gmail.com>
2022-11-30 17:40:29 +0900

    init repo

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+package $package;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+    @Test
+    void test() {
+        fail("not implemented");
+    }
+}
